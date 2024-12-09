8c9bb9ab13263187637a288a9e047f381bf89942
stas662 <Makarov.Stas.2018@gmail.com>
2024-12-09 22:05:17 +0200

    Added the ability to drag and drop multiple tasks

--- js/main.js
+++ js/main.js
@@ -1,38 +1,29 @@
-const item = document.querySelector('.item');
+const items = document.querySelectorAll('.item');
 const placeholders = document.querySelectorAll('.placeholder')
 
-item.addEventListener('dragstart', dragstart);
-item.addEventListener('dragend', dragend);
+for (const item of items) {
+    item.addEventListener('dragstart', (event) => {
+        let selected = event.target;
 
-for (const placeholder of placeholders) {
-    placeholder.addEventListener('dragover', dragover);
-    placeholder.addEventListener('dragenter', dragenter);
-    placeholder.addEventListener('dragleave', dragleave);
-    placeholder.addEventListener('drop', dragdrop);
+        for (const placeholder of placeholders) {
+            placeholder.addEventListener("dragover", (event) => {
+                event.preventDefault();
+            });
+            placeholder.addEventListener("dragenter", () => {
+                placeholder.classList.add('hovered');
+            });
+            placeholder.addEventListener("dragleave", () => {
+                placeholder.classList.remove('hovered');
+            });
+            placeholder.addEventListener("drop", () => {
+                placeholder.append(selected);
+                selected = '';
+            });
+        }
+    });
+
+    item.addEventListener('dragend', (event) => {
+        event.target.className = 'item';
+    });
 }
 
-function dragstart(event) {
-    event.target.classList.add('hold');
-    setTimeout(() => event.target.classList.add('hide'), 0);
-}
-
-function dragend(event) {
-    event.target.className = 'item';
-}
-
-function dragover(event) {
-    event.preventDefault();
-}
-
-function dragenter(event) {
-    event.target.classList.add('hovered');
-}
-
-function dragleave(event) {
-    event.target.classList.remove('hovered');
-}
-
-function dragdrop(event) {
-    event.target.classList.remove('hovered');
-    event.target.append(item);
-}
